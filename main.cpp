b55358f685ebe905c9e37867baae37d5e74f84a7
takayama <macho4287@gmail.com>
2016-11-15 17:18:45 +0900

    add the condition for incorrect arguments

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "minesweeper.hpp"
 
 int main(int argc, char* argv[]) {
-	
+
+	if(argc > 1 && argc != 4) {
+		printf("The number of arguments must be set to equal to 1 or 4.\n");
+		return -1;
+	}
+
 	int height = argc == 4 ? atoi(argv[1]) : 10;
 	int width = argc == 4 ? atoi(argv[2]) : 10;
 	int bombs = argc == 4 ? atoi(argv[3]) : 10;
-	if(bombs > height * width) {
-		printf("!!!too many bombs!!!\n");
-		return 0;
+	if(bombs < 0 || bombs > height * width) {
+		printf("The number of bombs must be set between 0 and the size of the map.\n");
+		return -1;
 	}
 
 	Mine mine(height, width, bombs);
--- makefile
+++ makefile
@@ -2,4 +2,4 @@
 # makefile for minesweeper
 mine: main.cpp minesweeper.cpp
 	g++ main.cpp minesweeper.cpp -o mine
-	cp mine /usr/local/bin
+	# cp mine /usr/local/bin
